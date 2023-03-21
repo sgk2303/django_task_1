4de6de5014141ee4c571b3ffbdc33511280fbfea
sgk2303 <gkvv2303@gmail.com>
2023-03-22 02:15:24 +0530

    Update views.py

--- employee_salary/views.py
+++ employee_salary/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.db.models import Max
+from .models import Department, Employee
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the Employee salary index.")
+def highest_paid_employee(request):
+    # Query the database for the highest paid employee in each department
+    departments = Department.objects.annotate(highest_salary=Max('employees__salary')).order_by('name')
+
+    # Create a list of dictionaries containing the highest paid employee in each department
+    results = []
+    for department in departments:
+        result = {}
+        highest_paid_employee = department.employees.filter(salary=department.highest_salary).first()
+        result['name'] = highest_paid_employee.name
+        result['department'] = department.name
+        result['salary'] = str(highest_paid_employee.salary)
+        results.append(result)
+
+    # Return the results as a JSON response
+    return JsonResponse({'results': results})
